37de772ead95c7652385c476f1093377c26af078
Abel <abelrosac@gmail.com>
2022-01-07 10:29:11 +0000

    Trabalho do curso
    primeiro commit no quasar project.

---
+++ src/pages/Index.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-page>
+<div class="q-pa-md">
+    <div class="q-col-gutter-md row items-start">
+      <div class="col-12">
+        <q-img src="~assets/site_quasar.jpg" style="height: 100%; max-width: 100%">
+          <div class="absolute-bottom text-subtitle1 text-center">
+            Site ainda em construçao
+          </div>
+        </q-img>
+      </div>
+    </div>
+</div>
+  </q-page>
+</template>
+<style>
+
+</style>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
